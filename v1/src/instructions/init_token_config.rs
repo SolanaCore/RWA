c056d0d102c8b22f659736455a88a8d8d18cbf1c
Dhruv Khandelwal <artificialintelligencehub35@gmail.com>
2025-09-06 20:30:50 +0530

    Update init_token_config.rs

--- v1/src/instructions/init_token_config.rs
+++ v1/src/instructions/init_token_config.rs
@@ -30,7 +30,7 @@ pub struct TokenConfigAccounts<'a> {
 /// Instruction wrapper for InitTokenConfig
 pub struct InitTokenConfigInstruction<'a> {
     pub accounts: TokenConfigAccounts<'a>,
-    pub instruction_datas: InitTokenConfig,
+    pub instruction_datas: TokenConfig,
 }
 
 impl<'a> TryFrom<&'a [AccountInfo<'a>]> for TokenConfigAccounts<'a> {
